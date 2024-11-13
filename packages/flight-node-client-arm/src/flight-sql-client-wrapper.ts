9e92fb4b73697e0f5d8f10c1ef41a2e14c5cffcb
David Estes <david@3box.io>
2024-11-13 12:43:38 -0700

    chore: delete duplicate flight sql package

--- packages/flight-node-client-arm/src/flight-sql-client-wrapper.ts
+++
@@ -1,9 +0,0 @@
-// flight-sql-client-wrapper.js
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const {
-  createFlightSqlClient,
-} = require("@lakehouse-rs/flight-sql-client");
-
-export { createFlightSqlClient };
